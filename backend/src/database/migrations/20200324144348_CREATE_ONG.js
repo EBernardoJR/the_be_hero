5ecae19d73985d73b6efde18b11a0617441d7729
EbernardoJR <emanoelbernardocontato@outlook.com>
2020-03-24 15:43:14 -0300

    first connection db

--- backend/src/database/migrations/20200324144348_CREATE_ONG.js
+++ backend/src/database/migrations/20200324144348_CREATE_ONG.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('ongs', function(table){
+  return knex.schema.createTable('ong', function(table){
     table.string('id').primary()
     table.string('name').notNullable()
     table.string('email').notNullable()
@@ -11,5 +11,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('ongs')//se der erro vai deletar a tabela
+  return knex.schema.dropTable('ong')//se der erro vai deletar a tabela
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,37 @@
 const express = require('express')
-
+const crypto = require('crypto')
+const connection = require('./database/connection')//conexão com o bd
 const routes = express.Router()
 
-routes.get('/user/:id', (req, res)=>{
-    //return res.send(req.params.id)
+//listagem das rotas
+routes.get('/ongs', async(req, res)=>{
+    const ongs = await connection('ongs').select('*') //vai selecionar todas as ongs
 
+    return res.json(ongs)
+} )
+
+
+//criar uma ong
+routes.post('/ongs', async (req, res)=>{
+    //return res.send(req.params.id)
+    const { name, email, whatsapp, city, uf } = req.body
+    const id = crypto.randomBytes(4).toString('HEX')//gerando id aleatorio
+    //inserindo os dados
+    await connection('ongs').insert({
+        id,
+        name, 
+        email,
+        whatsapp,
+        city,
+        uf
+    }).then(()=>{
+        console.log('ong cadastrada')
+    }).catch(err =>{
+        console.log(`Ocorreu um erro: ${err}`)
+    })
+
+
+    return res.json({ id })
 })
 
 module.exports = routes
